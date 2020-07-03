c2f9dfede2b3da1ce4f31e65a2ae06e72b367dc7
Pablo Obando <josepabloobandogonzalez@gmail.com>
2020-07-03 17:13:40 -0600

    Add global styles

---
+++ layouts/default.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <Nuxt keep-alive />
+  </div>
+</template>
+
+<style>
+html {
+  padding: 0;
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+    Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  color: #131415;
+}
+.container {
+  max-width: 50rem;
+  padding: 2rem 1rem;
+  min-height: 100vh;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  background-color: yellow;
+}
+</style>
